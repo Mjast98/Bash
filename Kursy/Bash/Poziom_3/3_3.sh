980cbe0d4e6fa851c94d49f54cb6da1d41b5e01e
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-03 17:45:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_3.sh
+++ Kursy/Bash/Poziom_3/3_3.sh
@@ -4,3 +4,10 @@ echo "==============================================="
 echo "If Then Else Else if"
 echo "==============================================="
 
+randomNum=$((( RANDOM % 10) +1 )) #szukana liczba
+
+if [ "$1" -eq "$randomNum" ] ; then
+    echo "Zgadles szukana liczba $randomNum"
+fi
+echo "Twoja liczba to: $1 a szukana to $randomNum"
+echo "==============================================="
