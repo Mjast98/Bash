11dc252f721dc5fa52cd5e1b4eaf439020092ae8
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-03 17:30:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_2.sh
+++ Kursy/Bash/Poziom_3/3_2.sh
@@ -1,1 +1,14 @@
-echo "Dzialam !"
+#!/bin/bash
+clear
+echo "==============================================="
+echo "Instrukcje warunkowe"
+echo "==============================================="
+#Prosta Gra w Zgadywanie Liczby.
+
+randomNum=$((( RANDOM % 10) +1 )) #szukana liczba
+
+if [ "$1" -eq "$randomNum" ] ; then
+    echo "Zgadles szukana liczba $randomNum"
+fi
+echo "Twoja liczba to: $1 a szukana to $randomNum"
+echo "==============================================="
