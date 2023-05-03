906c1e574c94eeb3338cc47fd9547f146f641ff9
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-03 16:15:01 +0000

    Update

--- Kursy/Bash/Poziom_2/2_16.sh
+++ Kursy/Bash/Poziom_2/2_16.sh
@@ -8,5 +8,15 @@ declare -p var1
 expr $var1 + 22
 var1="ss"
 expr $var1 + 22
+declare -i var2=22
 declare -p var2
-echo "Halo"
+expr $var2 + 22
+declare -p var2
+declare +i var2
+declare -p var2
+echo $var2
+expr $var2 + 10
+
+
+
+
