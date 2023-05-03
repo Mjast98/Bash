40f1228d67c02b23217b392362ac4edd7bd6d7bc
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-03 17:00:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_1.sh
+++ Kursy/Bash/Poziom_3/3_1.sh
@@ -3,4 +3,8 @@ clear
 echo "==============================================="
 echo "Przekazywanie zmiennych do skryptu w wieszu polecen"
 echo "==============================================="
-echo 
+echo "Podane elementy do skryptu to: $@"
+username=$1
+password=$2
+echo "Login:$username, Password:$password"
+echo "Nawiazano Polaczenie"
