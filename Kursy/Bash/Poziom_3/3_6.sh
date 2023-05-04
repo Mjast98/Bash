09db25ffa938517cbd8b6fcd97f2dab3d91986fe
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-04 14:00:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_6.sh
+++ Kursy/Bash/Poziom_3/3_6.sh
@@ -8,5 +8,7 @@ echo "Jakis tekst" > plik_z_tekstem.txt
 echo -ne "Prosze podaj liczbe ktora nie jest =0 albo =10\nLiczba: "; read number
 if [ $number -eq 0 ] || [ $number -eq 10 ]; then
     echo -e "Podales zabroniona liczbe: $number\nShame on you"
-    exit $number
+    exit $number # <- wstep do oblsugi bledow, $? bedzie rowny 10 lub 0
+elif [ $number -ge 5 ] && [ $number -le 20 ]; then
+    echo "Liczba: $number jest wieksza lub rowna 5 oraz mniejsza badz rowna 20"
 fi
