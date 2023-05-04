24c9500ed81b068e62d1c159d396a33f2e382b78
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-04 08:45:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_3.sh
+++ Kursy/Bash/Poziom_3/3_3.sh
@@ -10,6 +10,19 @@ if [ "$1" -eq "$randomNum" ] ; then
     echo "Zgadles szukana liczba $randomNum"
 else 
     echo -n "Nie zgadles liczby, masz dodatkowa probe. Podaj swoja liczbe: "
+    read newnumber
+    if [ $newnumber -eq $randomNum ] ; then
+        echo "Zgadles szukana liczba $randomNum"
+    elif [ $newnumber -lt $randomNum ] ; then
+        echo -ne "Otrzymales trzecia probe. Twoja liczba jest mniejsza od szukanej.\nPodaj swoja liczbe: "
+        read newnumber
+        if [ $newnumber -eq $randomNum ] ; then
+            echo "Zgadles szukana liczba $randomNum"
+        else
+            echo "No niestety, przegrales :/"
+        fi
+    fi    
+
 fi
-echo "Twoja liczba to: $1 a szukana to $randomNum"
+echo "Twoja liczba to $newnumber a szukana to $randomNum"
 echo "==============================================="
