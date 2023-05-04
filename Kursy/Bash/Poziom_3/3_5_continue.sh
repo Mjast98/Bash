1bb2db218ca41d2883c43e1f7063214bbf5b8fc1
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-04 10:00:01 +0000

    Update

--- Kursy/Bash/Poziom_3/3_5_continue.sh
+++ Kursy/Bash/Poziom_3/3_5_continue.sh
@@ -3,3 +3,12 @@ clear
 echo "==============================================="
 echo "Break i Continue"
 echo "==============================================="
+for element in $( ls $1 ); do
+    if [ "$element" = "home" ]; then
+        break;
+    fi
+    if [ "$element" = "example.sh" ]; then
+        continue;
+    fi
+    echo $element
+done
