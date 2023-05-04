2fedc2193fabf27bb93e9e104d6a7b9babebfe2e
Mjast98 <mjastrzebskiwork@gmail.com>
2023-05-04 14:15:01 +0000

    Update

--- Kursy/Bash/Poziom_4/4_1.sh
+++ Kursy/Bash/Poziom_4/4_1.sh
@@ -3,3 +3,20 @@ clear
 echo "==============================================="
 echo "Czytanie plikow, IFS i ograniczenia"
 echo "==============================================="
+file=./4_1_user_info.txt
+IFS="|"
+while read -r name surname address city;do
+    echo "Pan/Pani: $name"
+    echo "Nazwisko: $surname"
+    echo "Adres Zamieszkania: $address"
+    echo -e "Zapraszam do odwiedzenia miasta ${city}.\nZ wyrazami szacunku $name ${surname}."
+    echo ""
+done < $file
+# Sposob drugi z przekierowaniem pipe do petli while
+cat 4_1_user_info.txt | while read -r name surname address city; do
+    echo "Pan/Pani: $name"
+    echo "Nazwisko: $surname"
+    echo "Adres Zamieszkania: $address"
+    echo -e "Zapraszam do odwiedzenia miasta ${city}.\nZ wyrazami szacunku $name ${surname}."
+    echo ""
+done
